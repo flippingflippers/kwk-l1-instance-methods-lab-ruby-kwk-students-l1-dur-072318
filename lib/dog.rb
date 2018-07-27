3e0169a4b8a9a2f07242993ce6f0d7cf8e3ef948
flippingflippers <citrussunshine2004@gmail.com>
2018-07-27 14:38:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog 
+  def initialize(name)
+    @name = name
+  end
+  def bark
+    puts "Woof!"
+  end 
+  
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+
+# dog1 = Dog.new("Foofy")
+# dog2 = Dog.new("Coco")
+# dog1.bark
+# puts dog1.get_name
+# dog2.sit
+# puts dog2.get_name
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # person.rb
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
+
